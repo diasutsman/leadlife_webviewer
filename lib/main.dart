e3e862c0c07a831fae080f3e79efc05ea3902224
Dias Utsman <utsmand91@gmail.com>
2024-08-20 15:23:30 +0700

    "Removed InAppWebView and replaced with flutter_custom_tabs, updated pubspec.yaml and pubspec.lock accordingly."

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,14 @@
 import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_custom_tabs/flutter_custom_tabs.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
-    await InAppWebViewController.setWebContentsDebuggingEnabled(kDebugMode);
-  }
+  // if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
+  //   await InAppWebViewController.setWebContentsDebuggingEnabled(kDebugMode);
+  // }
 
   runApp(
     const MaterialApp(
@@ -29,25 +28,50 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final GlobalKey webViewKey = GlobalKey();
+  void _launchURL(BuildContext context) async {
+    final theme = Theme.of(context);
+    try {
+      await launchUrl(
+        Uri.parse(leadIdUrl),
+        customTabsOptions: CustomTabsOptions(
+          colorSchemes: CustomTabsColorSchemes.defaults(
+            toolbarColor: theme.colorScheme.surface,
+          ),
+          shareState: CustomTabsShareState.on,
+          urlBarHidingEnabled: false,
+          showTitle: false,
+        ),
+        safariVCOptions: SafariViewControllerOptions(
+          preferredBarTintColor: theme.colorScheme.surface,
+          preferredControlTintColor: theme.colorScheme.onSurface,
+          barCollapsingEnabled: true,
+          dismissButtonStyle: SafariViewControllerDismissButtonStyle.close,
+        ),
+      );
+    } catch (e) {
+      // If the URL launch fails, an exception will be thrown. (For example, if no browser app is installed on the Android device.)
+      debugPrint(e.toString());
+    }
+  }
 
   static const String leadIdUrl = "https://leadlife.id/";
 
-  InAppWebViewController? webViewController;
-  InAppWebViewSettings settings = InAppWebViewSettings(
-    isInspectable: kDebugMode,
-    mediaPlaybackRequiresUserGesture: false,
-    alwaysBounceHorizontal: false,
-    alwaysBounceVertical: false,
-    userAgent:
-        "Mozilla/5.0 (Linux; Android 9; LG-H870 Build/PKQ1.190522.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36",
-    supportMultipleWindows: true,
-    allowsInlineMediaPlayback: true,
-    iframeAllow: "camera; microphone",
-    iframeAllowFullscreen: true,
-    disallowOverScroll: true,
-  );
+  // InAppWebViewController? webViewController;
+  // InAppWebViewSettings settings = InAppWebViewSettings(
+  //   isInspectable: kDebugMode,
+  //   mediaPlaybackRequiresUserGesture: false,
+  //   alwaysBounceHorizontal: false,
+  //   alwaysBounceVertical: false,
+  //   userAgent:
+  //       "Mozilla/5.0 (Linux; Android 9; LG-H870 Build/PKQ1.190522.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36",
+  //   supportMultipleWindows: true,
+  //   allowsInlineMediaPlayback: true,
+  //   iframeAllow: "camera; microphone",
+  //   iframeAllowFullscreen: true,
+  //   disallowOverScroll: true,
+  // );
 
-  PullToRefreshController? pullToRefreshController;
+  // PullToRefreshController? pullToRefreshController;
   String url = "";
   double progress = 0;
   final urlController = TextEditingController();
@@ -55,174 +79,175 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-
-    pullToRefreshController = kIsWeb
-        ? null
-        : PullToRefreshController(
-            settings: PullToRefreshSettings(
-              color: Colors.blue,
-            ),
-            onRefresh: () async {
-              if (defaultTargetPlatform == TargetPlatform.android) {
-                webViewController?.reload();
-              } else if (defaultTargetPlatform == TargetPlatform.iOS) {
-                webViewController?.loadUrl(
-                    urlRequest:
-                        URLRequest(url: await webViewController?.getUrl()));
-              }
-            },
-          );
+    // pullToRefreshController = kIsWeb
+    //     ? null
+    //     : PullToRefreshController(
+    //         settings: PullToRefreshSettings(
+    //           color: Colors.blue,
+    //         ),
+    //         onRefresh: () async {
+    //           if (defaultTargetPlatform == TargetPlatform.android) {
+    //             webViewController?.reload();
+    //           } else if (defaultTargetPlatform == TargetPlatform.iOS) {
+    //             webViewController?.loadUrl(
+    //                 urlRequest:
+    //                     URLRequest(url: await webViewController?.getUrl()));
+    //           }
+    //         },
+    //       );
   }
 
   @override
   Widget build(BuildContext context) {
-    return PopScope(
-      canPop: false,
-      onPopInvokedWithResult: (didPop, result) async {
-        if ((await webViewController?.canGoBack()) == true) {
-          webViewController?.goBack();
-        }
-      },
-      child: Scaffold(
-        body: SafeArea(
-          child: Column(
-            children: [
-              Expanded(
-                child: InAppWebView(
-                  key: webViewKey,
-                  initialUrlRequest: URLRequest(
-                    url: WebUri(
-                      leadIdUrl,
-                      // "https://google.com"
-                    ),
-                  ),
-                  initialSettings: settings,
-                  pullToRefreshController: pullToRefreshController,
-                  onWebViewCreated: (controller) {
-                    webViewController = controller;
-                  },
-                  onLoadStart: (controller, url) {
-                    setState(() {
-                      this.url = url.toString();
-                      urlController.text = this.url;
-                    });
-                  },
-                  onPermissionRequest: (controller, request) async {
-                    return PermissionResponse(
-                        resources: request.resources,
-                        action: PermissionResponseAction.GRANT);
-                  },
-                  onCreateWindow: (controller, createWindowRequest) async {
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          insetPadding: EdgeInsets.zero,
-                          contentPadding: EdgeInsets.zero,
-                          backgroundColor: Colors.transparent,
-                          content: SizedBox(
-                            width: MediaQuery.of(context).size.width,
-                            height: MediaQuery.of(context).size.height,
-                            child: InAppWebView(
-                              // Setting the windowId property is important here!
-                              windowId: createWindowRequest.windowId,
-                              initialOptions: InAppWebViewGroupOptions(
-                                android: AndroidInAppWebViewOptions(
-                                  builtInZoomControls: true,
-                                  thirdPartyCookiesEnabled: true,
-                                ),
-                                crossPlatform: InAppWebViewOptions(
-                                    cacheEnabled: true,
-                                    javaScriptEnabled: true,
-                                    userAgent:
-                                        "Mozilla/5.0 (Linux; Android 9; LG-H870 Build/PKQ1.190522.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36"),
-                              ),
-                              onWebViewCreated:
-                                  (InAppWebViewController controller) {},
-                              onLoadStart: (InAppWebViewController controller,
-                                  WebUri? url) {
-                                print("onLoadStart popup $url");
-                              },
-                              onLoadStop: (InAppWebViewController controller,
-                                  WebUri? url) async {
-                                print("onLoadStop popup $url");
-                              },
-                              onCloseWindow: (controller) {
-                                // On Facebook Login, this event is called twice,
-                                // so here we check if we already popped the alert dialog context
-                                if (Navigator.canPop(context)) {
-                                  Navigator.pop(context);
-                                }
-                              },
-                            ),
-                          ),
-                        );
-                      },
-                    );
+    _launchURL(context);
+    return const Placeholder();
+    // return PopScope(
+    //   canPop: false,
+    //   onPopInvokedWithResult: (didPop, result) async {
+    //     if ((await webViewController?.canGoBack()) == true) {
+    //       webViewController?.goBack();
+    //     }
+    //   },
+    //   child: Scaffold(
+    //     body: SafeArea(
+    //       child: Column(
+    //         children: [
+    //           Expanded(
+    //             child: InAppWebView(
+    //               key: webViewKey,
+    //               initialUrlRequest: URLRequest(
+    //                 url: WebUri(
+    //                   leadIdUrl,
+    //                   // "https://google.com"
+    //                 ),
+    //               ),
+    //               initialSettings: settings,
+    //               pullToRefreshController: pullToRefreshController,
+    //               onWebViewCreated: (controller) {
+    //                 webViewController = controller;
+    //               },
+    //               onLoadStart: (controller, url) {
+    //                 setState(() {
+    //                   this.url = url.toString();
+    //                   urlController.text = this.url;
+    //                 });
+    //               },
+    //               onPermissionRequest: (controller, request) async {
+    //                 return PermissionResponse(
+    //                     resources: request.resources,
+    //                     action: PermissionResponseAction.GRANT);
+    //               },
+    //               onCreateWindow: (controller, createWindowRequest) async {
+    //                 showDialog(
+    //                   context: context,
+    //                   builder: (context) {
+    //                     return AlertDialog(
+    //                       insetPadding: EdgeInsets.zero,
+    //                       contentPadding: EdgeInsets.zero,
+    //                       backgroundColor: Colors.transparent,
+    //                       content: SizedBox(
+    //                         width: MediaQuery.of(context).size.width,
+    //                         height: MediaQuery.of(context).size.height,
+    //                         child: InAppWebView(
+    //                           // Setting the windowId property is important here!
+    //                           windowId: createWindowRequest.windowId,
+    //                           initialOptions: InAppWebViewGroupOptions(
+    //                             android: AndroidInAppWebViewOptions(
+    //                               builtInZoomControls: true,
+    //                               thirdPartyCookiesEnabled: true,
+    //                             ),
+    //                             crossPlatform: InAppWebViewOptions(
+    //                                 cacheEnabled: true,
+    //                                 javaScriptEnabled: true,
+    //                                 userAgent:
+    //                                     "Mozilla/5.0 (Linux; Android 9; LG-H870 Build/PKQ1.190522.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36"),
+    //                           ),
+    //                           onWebViewCreated:
+    //                               (InAppWebViewController controller) {},
+    //                           onLoadStart: (InAppWebViewController controller,
+    //                               WebUri? url) {
+    //                             print("onLoadStart popup $url");
+    //                           },
+    //                           onLoadStop: (InAppWebViewController controller,
+    //                               WebUri? url) async {
+    //                             print("onLoadStop popup $url");
+    //                           },
+    //                           onCloseWindow: (controller) {
+    //                             // On Facebook Login, this event is called twice,
+    //                             // so here we check if we already popped the alert dialog context
+    //                             if (Navigator.canPop(context)) {
+    //                               Navigator.pop(context);
+    //                             }
+    //                           },
+    //                         ),
+    //                       ),
+    //                     );
+    //                   },
+    //                 );
 
-                    return true;
-                  },
-                  shouldOverrideUrlLoading:
-                      (controller, navigationAction) async {
-                    var uri = navigationAction.request.url!;
+    //                 return true;
+    //               },
+    //               shouldOverrideUrlLoading:
+    //                   (controller, navigationAction) async {
+    //                 var uri = navigationAction.request.url!;
 
-                    if (![
-                      "http",
-                      "https",
-                      "file",
-                      "chrome",
-                      "data",
-                      "javascript",
-                      "about"
-                    ].contains(uri.scheme)) {
-                      if (await canLaunchUrl(uri)) {
-                        // Launch the App
-                        await launchUrl(
-                          uri,
-                        );
-                        // and cancel the request
-                        return NavigationActionPolicy.CANCEL;
-                      }
-                    }
+    //                 if (![
+    //                   "http",
+    //                   "https",
+    //                   "file",
+    //                   "chrome",
+    //                   "data",
+    //                   "javascript",
+    //                   "about"
+    //                 ].contains(uri.scheme)) {
+    //                   if (await canLaunchUrl(uri)) {
+    //                     // Launch the App
+    //                     await launchUrl(
+    //                       uri,
+    //                     );
+    //                     // and cancel the request
+    //                     return NavigationActionPolicy.CANCEL;
+    //                   }
+    //                 }
 
-                    return NavigationActionPolicy.ALLOW;
-                  },
-                  onLoadStop: (controller, url) async {
-                    pullToRefreshController?.endRefreshing();
-                    setState(() {
-                      this.url = url.toString();
-                      urlController.text = this.url;
-                    });
-                  },
-                  onReceivedError: (controller, request, error) {
-                    pullToRefreshController?.endRefreshing();
-                  },
-                  onProgressChanged: (controller, progress) {
-                    if (progress == 100) {
-                      pullToRefreshController?.endRefreshing();
-                    }
-                    setState(() {
-                      this.progress = progress / 100;
-                      urlController.text = url;
-                    });
-                  },
-                  onUpdateVisitedHistory: (controller, url, androidIsReload) {
-                    setState(() {
-                      this.url = url.toString();
-                      urlController.text = this.url;
-                    });
-                  },
-                  onConsoleMessage: (controller, consoleMessage) {
-                    if (kDebugMode) {
-                      print(consoleMessage);
-                    }
-                  },
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+    //                 return NavigationActionPolicy.ALLOW;
+    //               },
+    //               onLoadStop: (controller, url) async {
+    //                 pullToRefreshController?.endRefreshing();
+    //                 setState(() {
+    //                   this.url = url.toString();
+    //                   urlController.text = this.url;
+    //                 });
+    //               },
+    //               onReceivedError: (controller, request, error) {
+    //                 pullToRefreshController?.endRefreshing();
+    //               },
+    //               onProgressChanged: (controller, progress) {
+    //                 if (progress == 100) {
+    //                   pullToRefreshController?.endRefreshing();
+    //                 }
+    //                 setState(() {
+    //                   this.progress = progress / 100;
+    //                   urlController.text = url;
+    //                 });
+    //               },
+    //               onUpdateVisitedHistory: (controller, url, androidIsReload) {
+    //                 setState(() {
+    //                   this.url = url.toString();
+    //                   urlController.text = this.url;
+    //                 });
+    //               },
+    //               onConsoleMessage: (controller, consoleMessage) {
+    //                 if (kDebugMode) {
+    //                   print(consoleMessage);
+    //                 }
+    //               },
+    //             ),
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
